af6b0674ecd79d40f9491cbdd6df19a0d7dde5d7
traltb <traltb@fe.edu.vn>
2025-05-14 16:53:51 +0700

    add slot1 folder

---
+++ slot1/exercise1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>xin chào bạn</h1>
+      <h2>Hello React</h2>
+    </>
+    
+  );
+}
+
+export default App;
